b08b21cc6f5145fed8d8278ceaaaf263b1f993c7
Abdulrahman797 <70597324+Abdulrahman797@users.noreply.github.com>
2022-06-05 23:19:12 +0300

    Added new sign in page

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,7 +1,87 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/AuthContext";
+import styled from "styled-components";
+import Icon from "../../components/Icon/Icon";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
+const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(8.5px);
+  -webkit-backdrop-filter: blur(8.5px);
+  border-radius: 10px;
+  color: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 0.4rem;
+  background-image: url("https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?w=1380&t=st=1654460101~exp=1654460701~hmac=c09240ecd5fb2113666221181b82dec051bb2f7046e482b69aa5317f5ff6b62a");
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media only screen and (max-width: 320px) {
+    width: 80vw;
+    height: 90vh;
+    hr {
+      margin-bottom: 0.3rem;
+    }
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 360px) {
+    width: 100vw;
+    height: 100vh;
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 411px) {
+    width: 80vw;
+    height: 90vh;
+  }
 
+  @media only screen and (min-width: 768px) {
+    width: 80vw;
+    height: 80vh;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 70vw;
+    height: 50vh;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
+
+const WelcomeText = styled.h2`
+  margin: 3rem 0 2rem 0;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 30%;
+  width: 30%;
+`;
+
+const ButtonContainer = styled.div`
+  margin: 1rem 0 2rem 0;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ForgotPassword = styled.h4`
+  cursor: pointer;
+`;
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -22,38 +102,18 @@ const SignIn = () => {
   };
 
   return (
-    <div className="max-w-[700px] mx-auto my-16 p-4">
-      <div>
-        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
-        <p className="py-2">
-          Don't have an account yet?{" "}
-          <Link to="/signup" className="underline">
-            Sign up.
-          </Link>
-        </p>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Email Address</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-3"
-            type="email"
-          />
-        </div>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-3"
-            type="password"
-          />
-        </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Sign In
-        </button>
-      </form>
-    </div>
+    <MainContainer>
+      <WelcomeText>Hoş Geldiniz</WelcomeText>
+      <InputContainer>
+        <Input type="text" placeholder="Mail" />
+        <Input type="password" placeholder="Şifre" />
+      </InputContainer>
+      <ButtonContainer>
+        <Button content="Giriş Yap" />
+      </ButtonContainer>
+
+      <ForgotPassword>Şifreyi Unuttum ?</ForgotPassword>
+    </MainContainer>
   );
 };
 
